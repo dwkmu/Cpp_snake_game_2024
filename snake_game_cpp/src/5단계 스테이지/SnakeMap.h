e6a412d47a5dca2c8818022db58ef69988892aa7
DaeWoong99 <144864405+dwkmu@users.noreply.github.com>
2024-06-15 15:19:11 +0900

    Update SnakeMap.h

--- snake_game_cpp/src/5단계 스테이지/SnakeMap.h
+++ snake_game_cpp/src/5단계 스테이지/SnakeMap.h
@@ -1,94 +1,28 @@
-
-#include <iostream>
+#ifndef _SNAKEMAP_
+#define _SNAKEMAP_
 #include <vector>
-#include <ncurses.h>
-#include <cstdlib>
-#include <ctime>
-#include "SnakeMap.h"
-#ifndef SNAKEGAME_H
-#define SNAKEGAME_H
 
-
-
-struct CharPosition 
-{
-	int x, y;
-	CharPosition(int col, int row);
-	CharPosition();
-};
-
-class SnakeGame
-{
-private:
-  SnakeMap* snake_map;
-	int cou, cou2, cou3,cou4,cou5, del, maxwidth, maxheight;
-	char direction, partchar, edgechar, Growthchar, poisonchar, gatechar;
-	bool bEatsFruit, bEatsPoison;
-	CharPosition Growth; 
-	CharPosition poison;
-	CharPosition gate1; //gate1 추가
-	CharPosition gate2; //gate2 추가
-	CharPosition temphead; //게이트 이동 위치
-	CharPosition finish ; //마지막 이동지점 위치
-
-
-	std::vector<CharPosition> snake;
-	std::vector<std::vector<int> > map;
-	void InitGameWindow();
-	void DrawWindow();  
-	void DrawSnake();
-	void PositionGrowth();
-	void PositionPoison();
-	bool FatalCollision();
-	void MoveSnake();
-	bool GetsGrowth();
-	bool GetsPoison();
-	void makeGate1();
-	void makeGate2();
-
-	bool isPassed ;
-	bool isPassing;
-
-
-	int max_y;
-	int max_x;
-	void passing();
-
-	void checkDirection(CharPosition temphead);
-
-	
-
-//아래는 5단계 추가내용임.
-
-	void NextStage(); // 미션 성공 시 다음 스테이지 변경
-	bool GameWin; // 게임 승리 종료
-
-	void boardUpdate(); // 게임보드 업데이트
-	int currentStage;
-	void InitStage();
-
-
-	bool gateMissionClear;
-	bool poisonMissionClear;
-	bool growthMissionClear;
-	bool bodylenMissionClear;
-
-	int poisonN;
-	int growthN;
-	int gateN;
-	int bodylen;
-
-
-
+class SnakeMap {
 public:
-	SnakeGame();
-	~SnakeGame();
-	void PlayGame();
+    SnakeMap(const int stage);
+    int getTotalStageCount(); // 전체 게임 stage의 개수를 반환한다.
+    std::vector<std::vector<int> > getCurrentMap(); // currnet_map을 반환한다.
+    int getPositionInfo(const int x, const int y); // current_map[x][y]를 반환한다.
+    void setCurrentMap(const int next_stage); // currnet_stage를 next_stage로 set하고, current_map을 totalmap[next_stage]로 변경한다.
+    void setPositionInfo(const int x, const int y, const int Info);
 
-	//아래는 5단계 추가내용임.
 
-	
 
+
+
+
+private:
+    int MAP_WIDTH = 30;
+    int MAP_HEIGHT = 60;
+    int total_stage_count = 4;
+    std::vector<std::vector<std::vector<int> > > total_map;
+    int current_stage = 0;
+    std::vector<std::vector<int> > current_map;
 };
 
 #endif
